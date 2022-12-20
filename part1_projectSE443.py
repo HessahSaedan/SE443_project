afb9374e4c405b367e20caeae975971f55819e90
HessahSaedan <99714684+HessahSaedan@users.noreply.github.com>
2022-12-20 19:41:14 +0300

    Add files via upload

---
+++ part1_projectSE443.py
@@ -0,0 +1,43 @@
+import docker
+client = docker.from_env()
+#part 1
+client.swarm.leave(force = True)
+client.swarm.init()
+
+#Part B
+print("this is Part B")
+#print ID
+print("Swarm ID: ", client.swarm.attrs['ID'])
+#print Name
+print("Swarm Name: ", client.swarm.attrs['Spec']['Name'])
+#print Creation Date
+print("Swarm Creation Date: ", client.swarm.attrs['CreatedAt'])
+
+
+#part C 
+print("this is part C")
+client.networks.create("se443_test_net", driver = "overlay", scope ="global", 
+                       
+#part D
+ipam = docker.types.IPAMConfig(pool_configs = [docker.types.IPAMPool(subnet = "10.10.10.0/24")]))
+for net in client.networks.list():
+    if net.name == "se443_test_net":
+        print("Network ID: ", net.id)
+        print("Network Name: ", net.name)
+        print("Network Creation Date: ", net.attrs['Created'])
+print("this is part D")
+
+#part E
+print("this is part E")
+client.services.create("eclipse-mosquitto",name = "broker", restart_policy = docker.types.RestartPolicy(condition = "any")).scale(3) #restart "any" = always 
+print(client.services.list()[0].id)
+print(client.services.list()[0].name)
+print(client.services.list()[0].attrs['CreatedAt'])
+print(client.services.list()[0].attrs['Spec']['Mode']['Replicated']['Replicas'])
+
+
+
+# Removing Broker Service
+print("\nRemoving Broker service")
+client.services.get("broker").remove()
+print("Broker removed")
